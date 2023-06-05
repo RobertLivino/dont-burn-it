52f42ec694f7dc7489f91be7ec2f088e512b8bc3
robert livino <robertlivino08@gmail.com>
2023-06-05 20:26:47 -0300

    zepelin code

---
+++ dontBurnIt/Assets/Scripts/FisicClassScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FisicClassScript : MonoBehaviour
+{
+    public Vector3 accelerationVector;
+    public Vector3 velocityVector2;
+    // come�a chamando o primeiro frame atualizado    
+    void Start()
+    {
+    }
+    // Atualiza chamando uma vez por frame    
+    void FixedUpdate()
+    {
+        UpdateVelocity();
+        transform.position += velocityVector2 * Time.deltaTime + (accelerationVector * (Time.deltaTime * Time.deltaTime) / 2);
+    }
+    void UpdateVelocity()
+    {
+        velocityVector2 += accelerationVector * Time.deltaTime;
+    }
+}
